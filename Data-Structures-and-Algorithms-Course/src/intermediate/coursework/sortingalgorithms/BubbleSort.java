d727075be688a25a6c5bceb31c8baed7b050e217
Anksharskarp <aerialconnection98@gmail.com>
2022-05-21 21:04:04 -0400

    End of class covering algorithmic complexity and bubble sort.

---
+++ Data-Structures-and-Algorithms-Course/src/intermediate/coursework/sortingalgorithms/BubbleSort.java
@@ -0,0 +1,28 @@
+package intermediate.coursework.sortingalgorithms;
+
+class BubbleSort {
+    void bubbleSort(int arrayToSort[]) {
+        int n = arrayToSort.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arrayToSort[j] > arrayToSort[j + 1]) {
+                    int placeholderVariable = arrayToSort[j];
+                    arrayToSort[j] = arrayToSort[j + 1];
+                    arrayToSort[j + 1] = placeholderVariable;
+                }
+            }
+        }
+    }
+
+    void printArray(int sortedArray[]) {
+        int n = sortedArray.length;
+        for (int i = 0; i < n; ++i) {
+            System.out.print(sortedArray[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+    }
+}
+
